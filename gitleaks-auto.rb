dcd0fd261f87c71c52f8fb4b370c2837c4a1a40c
Duc Nguyen <anhducmata@gmail.com>
2025-06-22 13:16:22 +0700

    Update gitleaks-auto.rb

--- gitleaks-auto.rb
+++ gitleaks-auto.rb
@@ -1,7 +1,7 @@
 class GitleaksAuto < Formula
   desc "Install Gitleaks globally with Git pre-commit hook"
   homepage "https://github.com/anhducmata/gitleaks-auto"
-  url "https://github.com/anhducmata/gitleaks-auto/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/anhducmata/gitleaks-auto/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "fc9656ff9777b03fa0a1d95e776383f34f5fa7f03c96bfb7bb850a899fcf5e04"
   license "MIT"
 
